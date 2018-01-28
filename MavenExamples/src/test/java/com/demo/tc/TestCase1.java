3c31769f621310d4a806f5587ab191bc3c680bac
shyam4learing <shyam4learing@gmail.com>
2018-01-28 20:23:13 +0530

    local commit

---
+++ MavenExamples/src/test/java/com/demo/tc/TestCase1.java
@@ -0,0 +1,16 @@
+package com.demo.tc;
+
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+
+	
+	
+	@Test
+	public void tc1() 
+	{
+		for(int i=0;i<=100;i++)
+		System.out.println("this is first test case");
+	}
+	
+}
